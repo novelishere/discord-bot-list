70cc02e15168fdeea814bb1ecfcb8c72ac07e24a
Glitch (lemon-navy-colony) <none>
2022-05-01 19:28:41 +0000

    🍩⏲ Checkpoint ./komutlar/bot-ekle.js:43049364/197

--- komutlar/bot-ekle.js
+++ komutlar/bot-ekle.js
@@ -3,6 +3,8 @@ const db = require('quick.db')
 
 exports.run = async(client, message, args) => {
  
+  let id = args[0]
+  
   let msg = message.channel
   let modlog = client.channels.cache.get(process.env.MOD_LOG)
   let onayredlog = client.channels.cache.get(process.env.ONAY_RED_LOG)
@@ -10,25 +12,27 @@ exports.run = async(client, message, args) => {
   let user = db.fetch(`userbot_${message.author.id}`)
   let bot = db.fetch(`bot_${id}`)
   
-  let id = args[0]
   if(!id) return msg.send(`> Üzgünüm <@${message.author.id}>, **botunuzun idsini belirtmeniz gerekiyor.**`)
   if(isNaN(id)) return msg.send(`> Üzgünüm <@${message.author.id}>, **belirtmiş olduğunuz id rakamlardan oluşmuyor.**`)
   if(id.length !== 18) return msg.send(`> Üzgünüm <@${message.author.id}>, **discord idleri 18 uzunluktan oluşuyor.**`)
   
+  if(bot) {
   if(bot.status === 'bekliyor') return msg.send(`> Üzgünüm <@${message.author.id}>, bu bot zaten başvurmuş, lütfen onaylanmasını bekleyin.`)
   if(bot.status === 'onaylandı') return msg.send(`> Üzgünüm <@${message.author.id}>, bu bot zaten sistemimizde bulunuyor.`)
+  } else {
   
   const buttons = new MessageActionRow()
   .addComponents(
     
     new MessageButton()
 		.setURL(`https://discord.com/oauth2/authorize?client_id=${id}&guild_id=${message.guild.id}&scope=bot&permissions=0`)
-		.setLabel('0 Perm Davet')
+		.setLabel('Davet')
 		.setStyle('LINK'),
 
 		new MessageButton()
 		.setURL(`https://discord.com/oauth2/authorize?client_id=${id}&guild_id=${message.guild.id}&scope=bot&permissions=8`)
-		.setLabel('8 Perm Davet')
+		.setLabel('Davet')
+    .setEmoji(':eight: ')
 		.setStyle('LINK'),
 
 )
@@ -42,8 +46,8 @@ exports.run = async(client, message, args) => {
   
   msg.send(`> Hey <@${message.author.id}>, başvurunuz yetkililerle iletilmiştir. Lütfen onaylanmasını bekleyiniz.`)
   modlog.send(`<@${message.author.id}>, <@${id}> botunu sisteme ekledi.`)
-  onayredlog.send(``)
-  
+  onayredlog.send({embeds: [embed], components: [buttons]})
+}
 }
 exports.conf = {
   aliases: []
