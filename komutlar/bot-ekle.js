0f39f3149376554ef63e365ce35851ae72ff487a
Glitch (lemon-navy-colony) <none>
2022-05-01 19:06:19 +0000

    📡📰 Checkpoint ./komutlar/bot-ekle.js:43049364/856

--- komutlar/bot-ekle.js
+++ komutlar/bot-ekle.js
@@ -7,7 +7,7 @@ exports.run = async(client, message, args) => {
   let modlog = client.channels.cache.get(process.env.MOD_LOG)
   let onayredlog = client.channels.cache.get(process.env.ONAY_RED_LOG)
   
-  let user = db.fetch(`userbot_${message.author.id}_${id}`)
+  let user = db.fetch(`userbot_${message.author.id}`)
   let bot = db.fetch(`bot_${id}`)
   
   let id = args[0]
@@ -15,7 +15,17 @@ exports.run = async(client, message, args) => {
   if(isNaN(id)) return msg.send(`> Üzgünüm <@${message.author.id}>, **belirtmiş olduğunuz id rakamlardan oluşmuyor.**`)
   if(id.length !== 18) return msg.send(`> Üzgünüm <@${message.author.id}>, **discord idleri 18 uzunluktan oluşuyor.**`)
   
-  if(bot.status === 'bekliyor') return msg.send("> Üzgünüm <@${mess")
+  if(bot.status === 'bekliyor') return msg.send(`> Üzgünüm <@${message.author.id}>, bu bot zaten başvurmuş, lütfen onaylanmasını bekleyin.`)
+  if(bot.status === 'onaylandı') return msg.send(`> Üzgünüm <@${message.author.id}>, bu bot zaten sistemimizde bulunuyor.`)
+  
+  const embed = new MessageEmbed()
+  .setColor("BLUE")
+  .setAuthor({name: message.author.tag, iconURL: message.author.displayAvatarURL({dynamic: true})})
+  .addField(``,
+  
+  msg.send(`> Hey <@${message.author.id}>, başvurunuz yetkililerle iletilmiştir. Lütfen onaylanmasını bekleyiniz.`)
+  modlog.send(`<@${message.author.id}>, <@${id}> botunu sisteme ekledi.`)
+  onayredlog.send(``)
   
 }
 exports.conf = {
