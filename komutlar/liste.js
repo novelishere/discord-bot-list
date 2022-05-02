9a1216c4d78d75fe21b449150134862fefb4caea
Glitch (lemon-navy-colony) <none>
2022-05-02 12:21:25 +0000

    ⛵️😼 Checkpoint ./komutlar/liste.js:43049364/61

--- komutlar/liste.js
+++ komutlar/liste.js
@@ -5,7 +5,7 @@ exports.run = async(client, message, args) => {
   
   let msg = message.channel
   let list = db.fetch(`botlist_${message.guild.id}`) || 0
-  let liste = list.map(novel => novel).join("\n") || 0
+  let liste = Array.isArray(list)
   
   let perm = process.env.PERM_ROLE_ID
   if(!message.member.roles.cache.has(perm)) return msg.send(`> Üzgünüm <@${message.author.id}, **bu komut yanlız yöneticiler olarak ayarlı**`)
