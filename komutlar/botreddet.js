c6bbfba051fc0459c6d9cdb869780563ba1eaa99
Glitch (lemon-navy-colony) <none>
2022-05-03 10:39:03 +0000

    🐓🚀 Checkpoint ./komutlar/botonayla.js:43049364/2190 ./komutlar/botreddet.js:43049364/5982

---
+++ komutlar/botreddet.js
@@ -0,0 +1,30 @@
+const {MessageEmbed, MessageActionRow, MessageButton, Permissions} = require('discord.js')
+const db = require('orio.db')
+
+exports.run = async(client, message, args) => {
+  
+  let id = args[0]
+  let msg = message.channel
+  let log = client.channels.cache.get(process.env.MOD_LOG)
+  let bot = db.fetch(`bot_${id}`)
+  
+  let perm = process.env.PERM_ROLE_ID
+  if(!message.member.roles.cache.has(perm)) return msg.send(`> Üzgünüm <@${message.author.id}, **bu komut yanlız yöneticiler olarak ayarlı**`)
+  if(!id) return msg.send(`> Üzgünüm <@${message.author.id}>, **bir id belirtmen gerekiyor**`)
+  if(!bot) return msg.send(`> Böyle bir bot başvurmamış.`)
+  if(bot.status === 'onaylandı') return msg.send(`> Bu bot zaten sistemimizde bulunuyor.`)
+  
+  let s = args.slice(1).join(' ')
+  
+  msg.send(`> ✅ Tebrikler, botu reddetiniz.`)
+  log.send(`<@${bot.userId}> adlı kullanıcının botu <@${id}> reddedildi. \n\`\`\`\n${s || 'Sebep Girilmemiş'}\n\`\`\``)
+
+  db.set(`bot_${id}`,{status: 'reddedildi'})
+  
+}
+exports.conf = {
+  aliases: ['reddet']
+}
+exports.help = {
+  name: 'botreddet'
+}
