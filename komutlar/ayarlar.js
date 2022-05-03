1e33d9beb0e377398c1ee5fc7934c9497d6a8502
Glitch (lemon-navy-colony) <none>
2022-05-03 11:35:35 +0000

    👞🏑 Checkpoint ./komutlar/ayarlar.js:43049364/483

--- komutlar/ayarlar.js
+++ komutlar/ayarlar.js
@@ -6,10 +6,19 @@ exports.run = async (client, message, args) => {
   const {guild, channel, author} = message
   if (!message.member.permissions.has(Permissions.FLAGS.MANAGE_MESSAGES)) return channel.send(`> **Üzgünüm <@${author.id}>, bu komutu kullanmak için __Yetkili Olman__ gerekiyor.**`)
   
+  
+  let log = process.env.MOD_LOG || '❌ Ayarlanmamış'
+  let onayred = process.env.ONAY_RED_LOG || '❌ Ayarlanmamış.'
+  let kanal = process.env.CHANNEL || '❌ Ayarlanmamış'
+  let rol = process.env.PERM_ROLE_ID || '❌ Ayarlanmamış'
+  let ban = process.env.BAN || '❌ Aktif Değil.'
+  let otoonay = process.env.OTO_ONAY || '❌ Aktif Değil.'
+  
   const embed = new MessageEmbed()
   .setTitle("Ayarlar")
   .setColor("BLUE")
   .setFooter({text: author.tag, iconURL: author.displayAvatarURL({dynamic: true})})
+  .addField("",``)
   
 }
 exports.conf = {
